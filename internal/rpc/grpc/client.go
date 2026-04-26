d33c629f624062a18a86d31aca74fc6d6b7d861d
2005LIKHITH <likhithcvsrl@gmail.com>
2026-04-26 15:32:47 +0530

    fix: non-blocking gRPC dial for cluster peers

--- internal/rpc/grpc/client.go
+++ internal/rpc/grpc/client.go
@@ -24,12 +24,12 @@ type ReplicatedClient struct {
 }
 
 func NewReplicatedClient(addr string) (*ReplicatedClient, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 
 	conn, err := grpc.DialContext(ctx, addr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithBlock(),
+		grpc.WithReturnConnectionError(),
 	)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to %s: %w", addr, err)
